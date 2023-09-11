e01204105a5d84da6255736a52ae906641c729bd
Maria <mariakovshova777@gmail.com>
2023-09-11 21:20:56 +0300

    Main changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Header from './componets/header/Header'
+import Main from './componets/main/Main'
+import Footer from './componets/footer/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+        {/* <Header/> */}
+        <Main/>
+        {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
